58d54be2230e04188d32ae968028c80a8a620aea
Mesut Tasci <slackermesut@gmail.com>
2019-07-11 22:06:20 +0300

    Checker updated

--- checker.go
+++ checker.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -8,6 +9,17 @@ import (
 )
 
 type resultChan chan *Check
+
+type Check struct {
+	site     *Site
+	username string
+	found    bool // Keeps username is found or not on the website
+	failed   bool // Keeps check is success or not
+}
+
+func (c *Check) ProfileUrl() string {
+	return fmt.Sprintf(c.site.profileUrl, c.username)
+}
 
 type Checker struct {
 	username string
