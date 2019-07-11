8a03ddc5b7e1ed68dde3024a5e58d1c3d147d7d7
Mesut Tasci <slackermesut@gmail.com>
2019-07-11 12:57:28 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"sync"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	resultCh := make(chan Check, len(sites))
+	username := "mesuutt"
+
+	for i := 0; len(sites) > i; i++ {
+		wg.Add(1)
+		check := &Check{
+			username: username,
+			site:     &sites[i],
+		}
+		go checkSite(check, &wg, resultCh)
+	}
+	wg.Wait()
+	close(resultCh)
+
+	cyan := color.New(color.FgCyan).SprintFunc()
+	boldCyan := color.New(color.FgCyan).Add(color.Bold).SprintFunc()
+	boldRed := color.New(color.FgRed).Add(color.Bold).SprintFunc()
+	bolGreen := color.New(color.FgGreen).Add(color.Bold).SprintFunc()
+
+	for c := range resultCh {
+		if c.failed {
+			fmt.Printf("[%s] %s: %s (%s)\n", boldCyan("?"), boldCyan(c.site.name), c.ProfileUrl(), cyan("Check failed"))
+		} else {
+			if c.isFound {
+				fmt.Printf("[%s] %s: %s\n", bolGreen("+"), bolGreen(c.site.name), c.ProfileUrl())
+			} else {
+				fmt.Printf("[%s] %s: %s\n", boldRed("-"), boldRed(c.site.name), c.ProfileUrl())
+			}
+		}
+	}
+}
+
+func checkSite(check *Check, wg *sync.WaitGroup, resultCh chan Check) {
+	resp, err := http.Get(check.ProfileUrl())
+	defer wg.Done()
+
+	if err != nil {
+		// Check failed
+		check.failed = true
+		resultCh <- *check
+		return
+	}
+
+	defer resp.Body.Close()
+	if check.site.checkBy == "status_code" {
+		check.isFound = resp.StatusCode == 200
+		resultCh <- *check
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		check.failed = true
+		resultCh <- *check
+		return
+	}
+
+	check.isFound = !strings.Contains(string(body), check.site.errorMsg)
+	resultCh <- *check
+}
---
+++ result.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Site struct {
+	name       string
+	errorMsg   string
+	checkBy    string
+	rank       int
+	profileUrl string
+	mainUrl    string
+}
+
+type Check struct {
+	site     *Site
+	username string
+	isFound  bool
+	failed   bool // Keeps check is success or not
+}
+
+func (c *Check) ProfileUrl() string {
+	return fmt.Sprintf(c.site.profileUrl, c.username)
+}
