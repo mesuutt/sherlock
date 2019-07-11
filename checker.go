0fc5b4db157e6b1ad0357c06d7ae44d8c714912c
Mesut Tasci <slackermesut@gmail.com>
2019-07-11 18:13:32 +0300

    Checker moved to another file

---
+++ checker.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"sync"
+)
+
+type resultChan chan *Check
+
+type Checker struct {
+	username string
+	sites    []Site
+	results  resultChan
+	wg       *sync.WaitGroup
+}
+
+func newChecker(username string, sites *[]Site) *Checker {
+	return &Checker{
+		username: username,
+		results:  make(resultChan),
+		sites:    *sites,
+		wg:       &sync.WaitGroup{},
+	}
+}
+
+func (c *Checker) Check() {
+	for i := 0; len(c.sites) > i; i++ {
+		c.wg.Add(1)
+		check := &Check{
+			username: c.username,
+			site:     &sites[i],
+		}
+
+		go c.checkSite(check)
+	}
+	c.wg.Wait()
+	close(c.results)
+}
+
+func (c *Checker) Results() resultChan {
+	return c.results
+}
+
+func (c *Checker) checkSite(check *Check) {
+	resp, err := http.Get(check.ProfileUrl())
+	defer c.wg.Done()
+	if err != nil {
+		// Check failed
+		check.failed = true
+		c.results <- check
+		return
+	}
+
+	defer resp.Body.Close()
+	if check.site.checkBy == "status_code" {
+		check.found = resp.StatusCode == 200
+		c.results <- check
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		check.failed = true
+		c.results <- check
+		return
+	}
+
+	check.found = !strings.Contains(string(body), check.site.errorMsg)
+	c.results <- check
+}
--- main.go
+++ main.go
@@ -2,74 +2,63 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
-	"sync"
+	"os"
 
 	"github.com/fatih/color"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	resultCh := make(chan Check, len(sites))
-	username := "mesuutt"
+	// var wg sync.WaitGroup
+	// username := *flag.String("username", "", "One or more usernames to check with social networks.")
+	/* onlyFound := *flag.Bool("found", false, "Prints only found messages. Errors, and invalid username errors will not appear.")
+	flag.Parse()
 
-	for i := 0; len(sites) > i; i++ {
-		wg.Add(1)
-		check := &Check{
-			username: username,
-			site:     &sites[i],
-		}
-		go checkSite(check, &wg, resultCh)
+	if flag.NArg() == 0 {
+		printUsage()
+		os.Exit(1)
 	}
-	wg.Wait()
-	close(resultCh)
 
-	cyan := color.New(color.FgCyan).SprintFunc()
-	boldCyan := color.New(color.FgCyan).Add(color.Bold).SprintFunc()
-	boldRed := color.New(color.FgRed).Add(color.Bold).SprintFunc()
-	bolGreen := color.New(color.FgGreen).Add(color.Bold).SprintFunc()
+	username := flag.Args()[0]
+	*/
 
-	for c := range resultCh {
-		if c.failed {
-			fmt.Printf("[%s] %s: %s (%s)\n", boldCyan("?"), boldCyan(c.site.name), c.ProfileUrl(), cyan("Check failed"))
-		} else {
-			if c.found {
-				fmt.Printf("[%s] %s: %s\n", bolGreen("+"), bolGreen(c.site.name), c.ProfileUrl())
-			} else {
-				fmt.Printf("[%s] %s: %s\n", boldRed("-"), boldRed(c.site.name), c.ProfileUrl())
+	onlyFound := false
+	var rootCmd = &cobra.Command{
+		Use:     "sharlock",
+		Short:   "Find usernames across social networks",
+		Args:    cobra.MinimumNArgs(1),
+		Example: "sherlock mesuutt",
+		Run: func(cmd *cobra.Command, args []string) {
+			checker := newChecker(args[0], &sites)
+			go checker.Check()
+
+			cyan := color.New(color.FgCyan).SprintFunc()
+			boldCyan := color.New(color.FgCyan).Add(color.Bold).SprintFunc()
+			boldRed := color.New(color.FgRed).Add(color.Bold).SprintFunc()
+			bolGreen := color.New(color.FgGreen).Add(color.Bold).SprintFunc()
+
+			for c := range checker.Results() {
+				if onlyFound && (c.failed || !c.found) {
+					continue
+				}
+
+				if c.failed {
+					fmt.Printf("[%s] %s: %s (%s)\n", boldCyan("?"), boldCyan(c.site.name), c.ProfileUrl(), cyan("Check failed"))
+				} else {
+					if c.found {
+						fmt.Printf("[%s] %s: %s\n", bolGreen("+"), bolGreen(c.site.name), c.ProfileUrl())
+					} else {
+						fmt.Printf("[%s] %s: %s\n", boldRed("-"), boldRed(c.site.name), c.ProfileUrl())
+					}
+				}
 			}
-		}
-	}
-}
 
-func checkSite(check *Check, wg *sync.WaitGroup, resultCh chan Check) {
-	resp, err := http.Get(check.ProfileUrl())
-	defer wg.Done()
-
-	if err != nil {
-		// Check failed
-		check.failed = true
-		resultCh <- *check
-		return
+		},
 	}
 
-	defer resp.Body.Close()
-	if check.site.checkBy == "status_code" {
-		check.found = resp.StatusCode == 200
-		resultCh <- *check
-		return
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		check.failed = true
-		resultCh <- *check
-		return
-	}
-
-	check.found = !strings.Contains(string(body), check.site.errorMsg)
-	resultCh <- *check
 }
